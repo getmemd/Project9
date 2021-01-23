c6331d984fbccfe2de942a0ab8e718ed940a13ae
Adil <amedeuyev@gmail.com>
2021-01-23 13:10:11 +0600

    Initial Commit

---
+++ Project7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Адиль Медеуев on 23.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
