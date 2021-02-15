8d2f86778fabe7da9577515e64a0528043b6606c
Adil <amedeuyev@gmail.com>
2021-02-16 04:11:24 +0600

    First commit

---
+++ Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Адиль Медеуев on 23.01.2021.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Адиль Медеуев on 23.01.2021.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
